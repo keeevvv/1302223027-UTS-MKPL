419d80989ce0f0b2df51edfcae8f28fbca7321a1
KevZ12 <135513743+KevZ12@users.noreply.github.com>
2025-04-16 22:27:00 +0700

    perbaikan total tax anak pada TaxCalculator

--- TaxCalculator/src/main/java/TaxFunction/TaxFunction.java
+++ TaxCalculator/src/main/java/TaxFunction/TaxFunction.java
@@ -50,7 +50,7 @@ public class TaxFunction {
     private static int getFinalTax(boolean isMarried, int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, int numberOfChildren) {
         int tax;
         if (isMarried) {
-            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 4500000))));
 
         } else {
             tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
