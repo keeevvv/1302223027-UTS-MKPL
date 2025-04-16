7756634a05bee6235830ace2dc59e58646e07139
KevZ12 <135513743+KevZ12@users.noreply.github.com>
2025-04-16 19:36:10 +0700

    memperbaiki big class pada class Employee

---
+++ TaxCalculator/src/main/java/Employee/EmployeeSpouse.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Employee;
+
+/**
+ *
+ * @author kevin
+ */
+public class EmployeeSpouse {
+
+    private String spouseName;
+    private String spouseIdNumber;
+}
